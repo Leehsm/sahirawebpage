d0a300ee19b96afba2ee2b4f10b073991c26371e
Leehsm <shahzleehasimwork@gmail.com>
2022-12-28 17:40:03 +0800

    create footer and header

---
+++ resources/views/frontend/layout/header.blade.php
@@ -0,0 +1,63 @@
+<!-- Header -->
+<header class="header trans_300">
+    <!-- Top Navigation -->
+    <div class="top_nav">
+        <div class="container">
+        <div class="row">
+            <div class="col-md-6">
+            <div class="top_nav_left">Shopping With Us Now</div>
+            </div>
+            <div class="col-md-6 text-right">
+            <div class="top_nav_right">
+                <ul class="top_nav_menu">
+                <!-- Currency / Language / My Account -->
+                <li class="currency">
+                    <a href="#">
+                    outfitbysahira.com
+                    <i class="fa fa-shopping-bag"></i>
+                    </a>
+                </li>
+                </ul>
+            </div>
+            </div>
+        </div>
+        </div>
+    </div>
+    <div class="main_nav_container">
+        <div class="container">
+        <div class="row">
+            <div class="col-lg-12 text-right">
+            <div class="logo_container">
+                <a href="{{ url('/') }}">Sahi<span>Ra</span></a>
+            </div>
+            <nav class="navbar">
+                <ul class="navbar_menu">
+                <li><a href="{{ url('/') }}">Home</a></li>
+                <li><a href="/#aboutus">About Us</a></li>
+                <li><a href="/#ourproduct">Our Product</a></li>
+                <li><a href="/#contactus">Contact</a></li>
+                <li><a href="/#membership">Membership</a></li>
+                </ul>
+                <div class="hamburger_container">
+                <i class="fa fa-bars" aria-hidden="true"></i>
+                </div>
+            </nav>
+            </div>
+        </div>
+        </div>
+    </div>
+</header>
+
+<div class="hamburger_menu">
+    <div class="hamburger_close"><i class="fa fa-times" aria-hidden="true"></i></div>
+    <div class="hamburger_menu_content text-right">
+      <ul class="menu_top_nav">
+        <li class="menu_item"><a href="#">Home</a></li>
+        <li class="menu_item"><a href="#aboutus">About Us</a></li>
+        <li class="menu_item"><a href="#">Our Product</a></li>
+        <li class="menu_item"><a href="#">Contact</a></li>
+        <li class="menu_item"><a href="#">Membership</a></li>
+        <li class="menu_item"><a href="#">Shopping With Us</a></li>
+      </ul>
+    </div>
+</div>
