722e08c2b1eab977be18fbac88c7047df35d9b15
Leehsm <shahzleehasimwork@gmail.com>
2022-12-29 17:40:18 +0800

    create page for reading blog

---
+++ resources/views/frontend/menu/readblog.blade.php
@@ -0,0 +1,25 @@
+@extends('welcome')
+@section('frontend')
+@section('title')
+Blogs 
+@endsection
+
+<!-- Slider -->
+<div class="main_slider_blog">
+    <div class="container fill_height">
+        <div class="col">
+            <div class="main_slider_abt_blog" id="blog">
+                <h2>Blogs Blogs Blogs BlogsBlogs Blogs</h2  >
+                <h6>Date: </h6>
+                <hr>
+                <img src="assets/images/mainpage.jpg">
+                <hr>
+                <h6>We help you to change your outfit fashion and be more confident with yourself.</h6>
+            </div>
+        </div>
+    </div>
+</div>
+
+
+
+@endsection
