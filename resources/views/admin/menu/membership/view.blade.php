b75273b04621e325c6111eec22c4cde2b78f5682
Leehsm <shahzleehasimwork@gmail.com>
2022-12-29 17:43:21 +0800

    edit membership page admin page

--- resources/views/admin/menu/membership/view.blade.php
+++ resources/views/admin/menu/membership/view.blade.php
@@ -7,6 +7,12 @@ Membership
 <main class="col-md-8 ms-sm-auto col-lg-10 px-md-4">
   <div class="d-flex justify-content-between flex-wrap flex-md-nowrap align-items-center pt-3 pb-2 mb-3 border-bottom">
     <h1 class="h2">Membership</h1>
+      <div class="container-fluid">
+        <form class="d-flex" style="width: 30%" role="search">
+          <input class="form-control me-2" type="search" placeholder="Search" aria-label="Search">
+          <button class="btn btn-outline-success" type="submit">Search</button>
+        </form>
+      </div>
     <div class="btn-toolbar mb-2 mb-md-0">
       <div class="btn-group me-2" >
         <button  type="button" class="btn btn-sm btn-outline-secondary">
