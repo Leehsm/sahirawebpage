c58cb8a6bc6dd75ef6de84c59322167e70209b93
Leehsm <shahzleehasimwork@gmail.com>
2022-12-29 17:44:12 +0800

    add our team to sidebar

--- resources/views/admin/layout/sidebar.blade.php
+++ resources/views/admin/layout/sidebar.blade.php
@@ -4,15 +4,14 @@
     // dd($prefix);
 @endphp
 
-<div class="d-flex flex-column flex-shrink-0 p-3 text-bg-dark" style="width: 250px; height:150vh">
-    <a href="{{ url('admin/dashboard') }}" class="d-flex align-items-center mb-3 mb-md-0 me-md-auto text-white text-decoration-none">
-        <svg class="bi pe-none me-2" width="40" height="32"><use xlink:href="#bootstrap"></use></svg>
-        <span class="fs-4">Admin Page</span>
-    </a>
+<div class="d-flex flex-column flex-shrink-0 p-3 text-bg-dark" style="width: 250px; height:150vh; position: fixed;">
     <div class="dropdown">
+        <a href="{{ url('admin/dashboard') }}" class="d-flex align-items-center mb-3 mb-md-0 me-md-auto text-white text-decoration-none">
+            <svg class="bi pe-none me-2" width="40" height="32"></svg>
+            <span class="fs-4">Admin Page</span>
+        </a>
         <a href="#" class="d-flex align-items-center text-white text-decoration-none dropdown-toggle" data-bs-toggle="dropdown" aria-expanded="false">
-            <img src="https://github.com/mdo.png" alt="" width="32" height="32" class="rounded-circle me-2">
-            <strong>mdo</strong>
+            <strong></strong>
         </a>
         <ul class="dropdown-menu dropdown-menu-dark text-small shadow">
             <li><a class="dropdown-item" href="{{ route('admin.logout') }}">Sign out</a></li>
@@ -28,6 +27,11 @@
     <li class="nav-item">
         <a href="{{ route('aboutus.all') }}" class="nav-link text-white {{ ($prefix == '/aboutus')? 'active':'' }}" aria-current="page">
             About Us
+        </a>
+    </li>
+    <li>
+        <a href="{{ route('ourteam.all') }}" class="nav-link text-white {{ ($prefix == '/ourteam')? 'active':'' }}">
+            Our Teams
         </a>
     </li>
     <li>
