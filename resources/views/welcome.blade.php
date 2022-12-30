4290d73d7a0284a979e3580367c8dc0daf91c0d0
Leehsm <shahzleehasimwork@gmail.com>
2022-12-30 17:39:05 +0800

    add toastr cdn

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -146,8 +146,6 @@
     <link rel="stylesheet" type="text/css" href="assets/css/main_styles.css">
     <link rel="stylesheet" type="text/css" href="assets/css/responsive.css">
     
-    
-    
     <!-- Template Main JS File -->
     <script src="assets/js/jquery-3.2.1.min.js"></script>
     <script src="assets/js/popper.js"></script>
@@ -156,12 +154,42 @@
     <script src="assets/js/owl.carousel.js"></script>
     <script src="assets/js/easing.js"></script>
     <script src="assets/js/custom.js"></script>
+
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/toastr.js/latest/toastr.min.css" integrity="sha512-vKMx8UnXk60zUwyUnUPM3HbQo8QfmNx7+ltw8Pm5zLusl1XIfwcxo8DbWCqMGKaWeNxWA8yrx5v3SaVpMvR3CA==" crossorigin="anonymous" referrerpolicy="no-referrer" />
 </head>
 <body>
-<div class="super_container">
-  @include('frontend.layout.header')
-  @yield('frontend')
-  @include('frontend.layout.footer')
-</div>
+
+    <div class="super_container">
+    @include('frontend.layout.header')
+    @yield('frontend')
+    @include('frontend.layout.footer')
+    </div>
+
+    <script src="https://code.jquery.com/jquery-3.6.3.min.js" integrity="sha256-pvPw+upLPUjgMXY0G+8O0xUf+/Im1MZjXxxgOcBQBXU=" crossorigin="anonymous"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/toastr.js/latest/toastr.min.js" integrity="sha512-VEd+nq25CkR676O+pLBnDW09R7VQX9Mdiij052gVCp5yVH3jGtH70Ho/UUv4mJDsEdTvqRCFZg0NKGiojGnUCw==" crossorigin="anonymous" referrerpolicy="no-referrer"></script>
+    {{-- toastr notification --}}
+    <script>
+    @if(Session::has('message'))
+    var type = "{{ Session::get('alert-type','info') }}"
+    switch(type){
+        case 'info':
+        toastr.info("{{ Session::get('message') }}");
+        break;
+
+        case 'success':
+        toastr.success("{{ Session::get('message') }}");
+        break;
+
+        case 'warning':
+        toastr.warning("{{ Session::get('message') }}");
+        break;
+
+        case 'error':
+        toastr.error("{{ Session::get('message') }}");
+        break;
+    }
+
+    @endif
+    </script>
 </body>
 </html>
