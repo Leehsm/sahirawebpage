8cde330c9f5d352b100b29405d40b94967c3e17d
Leehsm <shahzleehasimwork@gmail.com>
2023-01-04 11:25:18 +0800

    retrieve our team data from db

--- resources/views/frontend/menu/aboutus.blade.php
+++ resources/views/frontend/menu/aboutus.blade.php
@@ -42,56 +42,6 @@ About Us
                     </div>
                 </div>
             @endforeach
-            
-            {{-- <div class="col-lg-4 blog_item_col">
-                <div class="blog_item">
-                <div class="blog_background" style="background-image:url(assets/images/blog_2.jpg)"></div>
-                <div class="blog_content d-flex flex-column align-items-center justify-content-center text-center">
-                    <h4 class="blog_title">Here are the trends I see coming this fall</h4>
-                    <span class="blog_meta">by admin | dec 01, 2021</span>
-                    <a class="blog_more" href="#">Read more</a>
-                </div>
-                </div>
-            </div>
-            <div class="col-lg-4 blog_item_col">
-                <div class="blog_item">
-                <div class="blog_background" style="background-image:url(assets/images/blog_3.jpg)"></div>
-                <div class="blog_content d-flex flex-column align-items-center justify-content-center text-center">
-                    <h4 class="blog_title">Here are the trends I see coming this fall</h4>
-                    <span class="blog_meta">by admin | dec 01, 2021</span>
-                    <a class="blog_more" href="#">Read more</a>
-                </div>
-                </div>
-            </div>
-            <div class="col-lg-4 blog_item_col">
-                <div class="blog_item">
-                <div class="blog_background" style="background-image:url(assets/images/slider_1.jpg)"></div>
-                <div class="blog_content d-flex flex-column align-items-center justify-content-center text-center">
-                    <h4 class="blog_title">Founder</h4>
-                    <span class="blog_meta">Isnaini Binti Moin</span>
-                </div>
-                </div>
-            </div>
-            <div class="col-lg-4 blog_item_col">
-                <div class="blog_item">
-                <div class="blog_background" style="background-image:url(assets/images/blog_2.jpg)"></div>
-                <div class="blog_content d-flex flex-column align-items-center justify-content-center text-center">
-                    <h4 class="blog_title">Here are the trends I see coming this fall</h4>
-                    <span class="blog_meta">by admin | dec 01, 2021</span>
-                    <a class="blog_more" href="#">Read more</a>
-                </div>
-                </div>
-            </div>
-            <div class="col-lg-4 blog_item_col">
-                <div class="blog_item">
-                <div class="blog_background" style="background-image:url(assets/images/blog_2.jpg)"></div>
-                <div class="blog_content d-flex flex-column align-items-center justify-content-center text-center">
-                    <h4 class="blog_title">Here are the trends I see coming this fall</h4>
-                    <span class="blog_meta">by admin | dec 01, 2021</span>
-                    <a class="blog_more" href="#">Read more</a>
-                </div>
-                </div>
-            </div> --}}
         </div>
     </div>
 </div>
