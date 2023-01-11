694a32d855d0557978be0f53b7878818a386b242
Leehsm <shahzleehasimwork@gmail.com>
2023-01-11 17:35:02 +0800

    edit readblog page

--- resources/views/frontend/menu/readblog.blade.php
+++ resources/views/frontend/menu/readblog.blade.php
@@ -9,17 +9,15 @@ Blogs
     <div class="container fill_height">
         <div class="col">
             <div class="main_slider_abt_blog" id="blog">
-                <h2>Blogs Blogs Blogs BlogsBlogs Blogs</h2  >
-                <h6>Date: </h6>
+                <h2>{{ $blogs->title }}</h2  >
+                <h6>Date: {{ $blogs->date }}</h6>
                 <hr>
-                <img src="assets/images/mainpage.jpg">
+                <img src="{{ asset($blogs->image) }}">
                 <hr>
-                <h6>We help you to change your outfit fashion and be more confident with yourself.</h6>
+                <h6>{{ $blogs->description }}</h6>
             </div>
         </div>
     </div>
 </div>
-
-
 
 @endsection
