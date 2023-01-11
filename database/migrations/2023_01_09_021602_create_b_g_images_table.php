5b1fc7d2dc9696b5adbd42f615260741feaaa287
Leehsm <shahzleehasimwork@gmail.com>
2023-01-11 17:33:50 +0800

    add migration file

--- database/migrations/2023_01_09_021602_create_b_g_images_table.php
+++ database/migrations/2023_01_09_021602_create_b_g_images_table.php
@@ -4,7 +4,7 @@ use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
-class CreateContactUsTable extends Migration
+class CreateBGImagesTable extends Migration
 {
     /**
      * Run the migrations.
@@ -13,11 +13,10 @@ class CreateContactUsTable extends Migration
      */
     public function up()
     {
-        Schema::create('contact_us', function (Blueprint $table) {
+        Schema::create('b_g_images', function (Blueprint $table) {
             $table->id();
             $table->string('name');
-            $table->string('desc_link');
-            $table->string('image')->nullable();
+            $table->string('image');
             $table->timestamps();
         });
     }
@@ -29,6 +28,6 @@ class CreateContactUsTable extends Migration
      */
     public function down()
     {
-        Schema::dropIfExists('contact_us');
+        Schema::dropIfExists('b_g_images');
     }
 }
