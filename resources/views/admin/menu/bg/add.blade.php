6842c3c271c33fe636e416d8c4ed7ee865377d4d
Leehsm <shahzleehasimwork@gmail.com>
2023-01-11 17:38:14 +0800

    bg crud

---
+++ resources/views/admin/menu/bg/add.blade.php
@@ -0,0 +1,36 @@
+@extends('admin.master')
+@section('content')
+@section('title')
+Add BG
+@endsection
+
+<main class="col-md-8 ms-sm-auto col-lg-10 px-md-4" style="width: -webkit-calc(100% - 200px); width: calc(100% - 200px); float:right;">  
+    <div class="d-flex justify-content-between flex-wrap flex-md-nowrap align-items-center pt-3 pb-2 mb-3 border-bottom">
+        <h1 class="h2">Add BG</h1>
+    </div>
+    <form method="post" action="{{ route('bg.store') }}" enctype="multipart/form-data">
+        @csrf
+        <div class="mb-3">
+            <label for="image" class="form-label">Image</label>
+            <input type="file" class="form-control" name="image" id="image">
+        </div>
+        <div class="mb-3">
+            <label for="name" class="form-label">Name</label>
+            <select id="name" name="name" class="form-select">
+                <option value="about us">About Us</option>
+                <option value="contact us">Contact Us</option>
+                <option value="membership">Membership</option>
+                <option value="blog">Blog</option>
+                <option value="faq">FAQ</option>
+            </select>
+        </div>
+        <div class="mb-3">
+            <label for="description" class="form-label">Description</label>
+            <textarea class="form-control" name="description" id="description" rows="3"></textarea>
+        </div>
+        <div class="text-xs-right">
+            <input type="submit" class="btn btn-rounded btn-primary mb-5" value="Add New">					 
+        </div>
+    </form>
+</main>
+@endsection
