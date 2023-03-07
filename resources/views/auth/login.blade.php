5e89b5668b9fd98ebcc7db222f0184e72a52005f
Leehsm <shahzleehasimwork@gmail.com>
2023-03-07 17:39:42 +0800

    simple edit

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -5,7 +5,7 @@
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
 
-    <title>Laravel 8 Custom 404 Page Example</title>
+    <title>404 </title>
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css">
 
     <style type="text/css">
